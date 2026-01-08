ff205b4a6b489ae91c27221667b11eb6e768ab67
aibayanyu20 <aibayanyu@qq.com>
2026-01-08 09:32:06 +0800

    chore: change

--- playground/src/pages/components/float-button/demo/group.vue
+++ playground/src/pages/components/float-button/demo/group.vue
@@ -1,9 +1,9 @@
 <docs lang="zh-CN">
-按钮组合使用时，推荐使用 `<FloatButton.Group />`，并通过设置 `shape` 属性改变悬浮按钮组的形状。悬浮按钮组的 `shape` 会覆盖内部 FloatButton 的 `shape` 属性。
+按钮组合使用时，推荐使用 `<a-float-button-group />`，并通过设置 `shape` 属性改变悬浮按钮组的形状。悬浮按钮组的 `shape` 会覆盖内部 FloatButton 的 `shape` 属性。
 </docs>
 
 <docs lang="en-US">
-When multiple buttons are used together, `<FloatButton.Group />` is recommended. By setting the `shape` property of FloatButton.Group, you can change the shape of group. The `shape` of the FloatButton.Group will override the `shape` of FloatButtons inside.
+When multiple buttons are used together, `<a-float-button-group />` is recommended. By setting the `shape` property of FloatButton.Group, you can change the shape of group. The `shape` of the FloatButton.Group will override the `shape` of FloatButtons inside.
 </docs>
 
 <script setup lang="ts">
@@ -18,7 +18,7 @@ import { QuestionCircleOutlined, SyncOutlined } from '@antdv-next/icons'
       </template>
     </a-float-button>
     <a-float-button />
-    <a-float-button-back-top :visibility-height="0" />
+    <a-float-back-top :visibility-height="0" />
   </a-float-button-group>
   <a-float-button-group shape="square" style="inset-inline-end: 94px;">
     <a-float-button>
@@ -32,6 +32,6 @@ import { QuestionCircleOutlined, SyncOutlined } from '@antdv-next/icons'
         <SyncOutlined />
       </template>
     </a-float-button>
-    <a-float-button-back-top :visibility-height="0" />
+    <a-float-back-top :visibility-height="0" />
   </a-float-button-group>
 </template>
