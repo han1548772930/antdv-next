acab3c0cbf1bf0631d73e9e2c801f2479dffa6f2
aibayanyu20 <aibayanyu@qq.com>
2026-01-28 16:07:35 +0800

    ci: update workflow add test ci

--- vitest-plugin.ts
+++ vitest-plugin.ts
@@ -1,7 +1,11 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 import { tsxResolveTypes } from 'vite-plugin-tsx-resolve-types'
+
+const baseUrl = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   plugins: [
@@ -20,4 +24,16 @@ export default defineConfig({
       },
     },
   ],
+  resolve: {
+    alias: [
+      {
+        find: /^antdv-next/,
+        replacement: path.resolve(baseUrl, './packages/antdv-next/src'),
+      },
+      {
+        find: /^@antdv-next\/cssinjs/,
+        replacement: path.resolve(baseUrl, './packages/cssinjs/src'),
+      },
+    ],
+  },
 })
